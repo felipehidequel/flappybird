335efa341bdfeb8c576ed82cc663f2e688e644f7
Felipe Hidequel <felipehidequel@gmail.com>
2023-08-09 22:26:10 -0300

    added desenhar_tela() function.

--- FlappyBird.py
+++ FlappyBird.py
@@ -153,3 +153,15 @@ class chao:
     def desenhar(self, tela):
         tela.blit(self.IMAGEM, (self.x1, self.y))
         tela.blit(self.IMAGEM, (self.x2, self.y))
+
+def desenhar_tela(tela, passaros, canos, chao, pontos):
+    tela.blit(IMAGEM_BACKGROUND, (0,0))
+    for passaro in passaros:
+        passaro.desenhar(tela)
+    for cano in canos:
+        cano.desenhar(tela)
+    
+    texto = FONTE_PONTOS.render(f"Pontuação: {pontos}", 1, (255,255,255))
+    tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width), 10)
+    chao.desenhar(tela)
+    pygame.display.update()
